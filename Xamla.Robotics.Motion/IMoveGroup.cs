1b48c77e5139320af1b17bd13c314c0725b0dbf7
Alexandre Stueben <Alexandre.Stueben@mailbox.org>
2018-10-16 14:53:24 +0200

    Added documentation to MoveGroup; adjusted documentation of IMoveGroup

--- Xamla.Robotics.Motion/IMoveGroup.cs
+++ Xamla.Robotics.Motion/IMoveGroup.cs
@@ -12,9 +12,8 @@ namespace Xamla.Robotics.Motion
     public interface IMoveGroup : IDisposable
     {
         /// <summary>
-        /// Name of the <c>IMoveGroup</c>.
+        /// Name of the <c>IMoveGroup</c>
         /// </summary> 
-
         string Name { get; }
 
         /// <summary>
@@ -30,7 +29,8 @@ namespace Xamla.Robotics.Motion
 
         /// <summary>
         /// Sets the collision check
-        /// If true, the trajectory planing tries to plan a collision free trajectory. Before executing a trajectory, a collision check is performed.
+        /// If true, the trajectory planing tries to plan a collision free trajectory. 
+        /// Before executing a trajectory, a collision check is performed.
         /// </summary> 
         bool CollisionCheck { get; set; }
 
@@ -50,8 +50,8 @@ namespace Xamla.Robotics.Motion
         double IkJumpThreshold { get; set; }
 
         /// <summary>
-        /// List of endeffector names the move group contains
-        /// </summary>   
+        /// Object implementing <c>IEnumerable<string></c> containing endeffector names of the the move group
+        /// </summary>  
         IEnumerable<string> EndEffectorNames { get; }
 
         /// <summary>
@@ -60,22 +60,22 @@ namespace Xamla.Robotics.Motion
         string DefaultEndEffectorName { get; set; }
 
         /// <summary>
-        /// Get a reference to an object implementing <c>IEndEffector</c>. 
+        /// Get an end effector by name 
         /// </summary>  
-        /// <param name="endEffectorName">Name of the end effector The default end effector is used when null.</param>  
-        /// <returns>Returns the object implementig <c>IEndEffector</c> associated with <c>endEffectorName</c>.</returns>  
+        /// <param name="endEffectorName">Name of the end effector. The default end effector is used when null.</param>  
+        /// <returns>Returns an object implementing <c>IEndEffector</c> associated with  <paramref name="endEffectorName"/>.</returns>  
         IEndEffector GetEndEffector(string endEffectorName = null);
 
         /// <summary>
-        /// Default end effector
+        /// Get default end effector
         /// </summary>  
         IEndEffector DefaultEndEffector { get; }
 
         /// <summary>
-        /// Get the current <c>Pose</c> instance of an end effector.  
+        /// Get the current position of an end effector
         /// </summary>  
         /// <param name="endEffectorName">Name of the <c>IEndEffector</c> instance. The default end effector is used when null.</param>  
-        /// <returns>Returns the current <c>Pose</c> instance of the end effector associated with <c>endEffectorName</c>.</returns>  
+        /// <returns>Returns the current <c>Pose</c> instance of the end effector associated with <paramref name="endEffectorName"/>.</returns>  
         Pose GetCurrentPose(string endEffectorName = null);
 
         /// <summary>
@@ -89,7 +89,7 @@ namespace Xamla.Robotics.Motion
         JointStates CurrentJointStates { get; }
 
         /// <summary>
-        /// Current joint positions of the of the <c>IMoveGroup</c> joints
+        /// Current joint positions of the of the <c>IMoveGroup</c> joints 
         /// </summary>  
         JointValues CurrentJointPositions { get; }
 
@@ -100,12 +100,12 @@ namespace Xamla.Robotics.Motion
 
         /// <summary>
         /// Builds an instance of <c>PlanParameters</c>.
-        /// For parameter == null, the default values in DefaultPlanParameters are used. 
+        /// For parameter == null, the default values in <c>DefaultPlanParameters</c> are used. 
         /// </summary>  
         /// <param name="velocityScaling">Scaling factor which is applied on the maximal possible joint velocities</param>  
         /// <param name="collisionCheck">Activates/Deactivates collision check</param>  
         /// <param name="maxDeviation">Maximal deviation from trajectory points when it is a fly-by-point in joint space</param>  
-        /// <param name="accelerationScaling">Scaling factor which is applied on the maximal possible joint accelerations.</param>  
+        /// <param name="accelerationScaling">Scaling factor which is applied on the maximal possible joint accelerations</param>  
         /// <returns>Returns a new <c>PlanParameters</c> instance based on <c>DefaultPlanParameters</c> and parameters.</returns>  
         PlanParameters BuildPlanParameters(double? velocityScaling = null, bool? collisionCheck = null, double? maxDeviation = null, double? accelerationScaling = null);
        
@@ -113,9 +113,9 @@ namespace Xamla.Robotics.Motion
         /// Builds an instance of <c>TaskSpacePlanParameters</c>. 
         /// </summary>  
         /// <param name="velocityScaling">Scaling factor which is applied on the maximal possible joint velocities</param>  
-        /// <param name="collisionCheck">Activates/Deactivates collision check</param>  
+        /// <param name="collisionCheck"> If true the trajectory planing tries to plan a collision free trajectory and before executing a trajectory a collision check is performed.</param>  
         /// <param name="maxDeviation">Maximal deviation from trajectory points when it is a fly-by-point in joint space</param>  
-        /// <param name="accelerationScaling">Scaling factor which is applied on the maximal possible joint accelerations.</param>  
+        /// <param name="accelerationScaling">Scaling factor which is applied on the maximal possible joint accelerations</param>  
         /// <param name="endEffectorName">Name of the end effector</param>  
         /// <returns>Returns a new <c>TaskSpacePlanParameters</c> instance.</returns>  
         TaskSpacePlanParameters BuildTaskSpacePlanParameters(double? velocityScaling = null, bool? collisionCheck = null, double? maxDeviation = null, double? accelerationScaling = null, string endEffectorName = null);
@@ -137,7 +137,7 @@ namespace Xamla.Robotics.Motion
         /// <param name="velocityScaling">Scaling factor which is applied on the maximal possible joint velocities</param>  
         /// <param name="collisionCheck">Activates/Deactivates collision check</param>  
         /// <param name="maxDeviation">Maximal deviation from trajectory points when it is a fly-by-point in joint space</param>  
-        /// <param name="accelerationScaling">Scaling factor which is applied on the maximal possible joint accelerations.</param>  
+        /// <param name="accelerationScaling">Scaling factor which is applied on the maximal possible joint accelerations</param>  
         /// <returns>Returns a tuple of <c>(IJointTrajectory, PlanParameters)</c> instances.</returns>  
         (IJointTrajectory, PlanParameters) PlanMoveJoints(JointValues target, double? velocityScaling = null, bool? collisionCheck = null, double? accelerationScaling = null);
       
@@ -177,7 +177,7 @@ namespace Xamla.Robotics.Motion
         /// <param name="velocityScaling">Scaling factor which is applied on the maximal possible joint velocities</param>  
         /// <param name="collisionCheck">Activates/Deactivates collision check</param>  
         /// <param name="maxDeviation">Maximal deviation from trajectory points when it is a fly-by-point in joint space</param>  
-        /// <param name="accelerationScaling">Scaling factor which is applied on the maximal possible joint accelerations.</param>  
+        /// <param name="accelerationScaling">Scaling factor which is applied on the maximal possible joint accelerations</param>  
         /// <returns>Returns a tuple of <c>(IJointTrajectory, PlanParameters)</c> instances.</returns>  
         (IJointTrajectory, PlanParameters) PlanMoveJointPath(IJointPath waypoints, double? velocityScaling = null, bool? collisionCheck = null, double? maxDeviation = null, double? accelerationScaling = null);
 
@@ -196,7 +196,7 @@ namespace Xamla.Robotics.Motion
         /// <param name="velocityScaling">Scaling factor which is applied on the maximal possible joint velocities</param>  
         /// <param name="collisionCheck">Activates/Deactivates collision check</param>  
         /// <param name="maxDeviation">Maximal deviation from trajectory points when it is a fly-by-point in joint space</param>  
-        /// <param name="accelerationScaling">Scaling factor which is applied on the maximal possible joint accelerations.</param>  
+        /// <param name="accelerationScaling">Scaling factor which is applied on the maximal possible joint accelerations</param>  
         /// <param name="cancel">CancellationToken</param>  
         /// <returns>Returns a <c>Task</c> instance.</returns>  
         Task MoveJointPathAsync(IJointPath waypoints, double? velocityScaling = null, bool? collisionCheck = null, double? maxDeviation = null, double? accelerationScaling = null, CancellationToken cancel = default(CancellationToken));
@@ -216,7 +216,7 @@ namespace Xamla.Robotics.Motion
         /// <param name="velocityScaling">Scaling factor which is applied on the maximal possible joint velocities</param>  
         /// <param name="collisionCheck">Activates/Deactivates collision check</param>  
         /// <param name="maxDeviation">Maximal deviation from trajectory points when it is a fly-by-point in joint space</param>  
-        /// <param name="accelerationScaling">Scaling factor which is applied on the maximal possible joint accelerations.</param>  
+        /// <param name="accelerationScaling">Scaling factor which is applied on the maximal possible joint accelerations</param>  
         /// <param name="cancel">CancellationToken</param>  
         /// <returns>Returns an object implementing <c>ISteppedMotionClient</c>.</returns>  
         ISteppedMotionClient MoveJointPathSupervisedAsync(IJointPath waypoints, double? velocityScaling = null, bool? collisionCheck = null, double? maxDeviation = null, double? accelerationScaling = null, CancellationToken cancel = default(CancellationToken));
