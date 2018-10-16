bc092bac54ff3b0a68fad565695bcb93aedbbc98
Alexandre Stueben <Alexandre.Stueben@mailbox.org>
2018-10-16 13:17:47 +0200

    Added documentation for SteppedMotionClient

--- Xamla.Robotics.Motion/SteppedMotionClient.cs
+++ Xamla.Robotics.Motion/SteppedMotionClient.cs
@@ -16,6 +16,9 @@ namespace Xamla.Robotics.Motion
 {
     using xamlamoveit = Messages.xamlamoveit_msgs;
 
+    /// <summary>
+    /// Client to perform supervised trajectory execution
+    /// </summary>
     public class SteppedMotionClient
         : ISteppedMotionClient
     {
@@ -37,6 +40,18 @@ namespace Xamla.Robotics.Motion
         private IDisposable cancelSubscription;
         private double velocityScaling;
 
+
+        /// <summary>
+        /// Creates a new <c>SteppedMotionClient</c>
+        /// </summary>
+        /// <param name="nodeHandle">Handle of a ROS node</param>
+        /// <param name="trajectory">Trajectory which should be executed supervised</param>
+        /// <param name="velocityScaling">Scaling factor to reduce or increase the trajectory velocities range [0.0 - 1.0]</param>
+        /// <param name="checkCollision">If true collision check is performed</param>
+        /// <param name="cancel">CancellationToken</param>
+        /// <returns>An instance of <c>StepMotionClient</c>.</returns>
+        /// <exception cref="Exception">Thrown when ActionSever is not available.</exception>
+        /// TODO: might throw more exceptions
         public SteppedMotionClient(NodeHandle nodeHandle, IJointTrajectory trajectory, double velocityScaling, bool checkCollision, CancellationToken cancel = default(CancellationToken))
         {
             this.nodeHandle = nodeHandle;
@@ -65,30 +80,52 @@ namespace Xamla.Robotics.Motion
             feedbackSubscriber = nodeHandle.Subscribe<xamlamoveit.TrajectoryProgress>(FEEDBACK_TOPIC, 10, OnFeedbackMessage);
         }
 
+        /// <summary>
+        /// Current ros action goal id
+        /// </summary>
         public string GoalId =>
             this.goalId.id;
 
+        /// <summary>
+        /// Current state of the supervised trajectory execution
+        /// </summary>
         public SteppedMotionState MotionState =>
             this.motionState;
 
+        /// <summary>
+        /// The goal task as an instance of <c>Task</c> producing an instance of <c>xamlamoveit.StepwiseMoveJResult</c> as result.
+        /// </summary>
         public Task<xamlamoveit.StepwiseMoveJResult> GoalTask =>
             this.goalHandle.GoalTask;
 
+        /// <summary>
+        /// Request at supervised executor to perform next step 
+        /// </summary>
         public void Next()
         {
             nextPublisher.Publish(goalId);
         }
 
+        /// <summary>
+        /// Request at supervised executor to perform previous step 
+        /// </summary>
         public void Previous()
         {
             previousPublisher.Publish(goalId);
         }
 
+        /// <summary>
+        /// Callback function reacting to feedback message
+        /// </summary>
+        /// <param name="feedback">The feedback message indicating the progress of the trajectory</param>
         private void OnFeedbackMessage(xamlamoveit.TrajectoryProgress feedback)
         {
             this.motionState = new SteppedMotionState(this.goalId.id, feedback.error_msg, (int)feedback.error_code, feedback.progress);
         }
 
+        /// <summary>
+        /// Clean up
+        /// </summary>
         public void Dispose()
         {
             feedbackSubscriber?.Dispose();
