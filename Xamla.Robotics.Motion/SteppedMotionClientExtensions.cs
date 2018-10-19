40c3aaad635754424704acf9e4c01c5d04d6e968
Alexandre Stueben <Alexandre.Stueben@mailbox.org>
2018-10-19 11:57:55 +0200

    Added documentation for SteppedMotionClientExtensions

--- Xamla.Robotics.Motion/SteppedMotionClientExtensions.cs
+++ Xamla.Robotics.Motion/SteppedMotionClientExtensions.cs
@@ -5,8 +5,16 @@ using Uml.Robotics.Ros;
 
 namespace Xamla.Robotics.Motion
 {
+    /// <summary>
+    /// Extensions of the <c>ISteppedMotionClient</c> interface
+    /// </summary>
     public static class SteppedMotionClientExtensions
     {
+        /// <summary>
+        /// Extends the interface <c>ISteppedMotionClient</c> with a method <c>ToModel</c>, which creates an instance of <c>SteppedMotionClientModel</c> from a state snapshot.
+        /// </summary>
+        /// <param name="client">The client to be extended</param>
+        /// <returns>Returns an instance of  <c>SteppedMotionClientModel</c>.</returns>
         public static SteppedMotionClientModel ToModel(this ISteppedMotionClient client)
         {
             var state = client.MotionState;     // read immutable state snapshot once
