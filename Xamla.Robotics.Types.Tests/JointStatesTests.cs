7a6dd545a1367b45c4cf337b418cae5098d09971
Alexandre Stueben <Alexandre.Stueben@mailbox.org>
2018-10-30 16:10:23 +0100

    Renamed files

--- Xamla.Robotics.Types.Tests/JointStatesTests.cs
+++ Xamla.Robotics.Types.Tests/JointStatesTests.cs
@@ -8,16 +8,16 @@ namespace Xamla.Robotics.Types.Tests
         public void TestInit()
         {
 
-            var pos = new JointValues(new JointSet("a" , "b","c"), new double[] {2, 3 ,4});
-            var vel = new JointValues(new JointSet("a" , "b","c"), new double[] {2, 3 ,4});
-            var eff = new JointValues(new JointSet("a" , "b","c"), new double[] {2, 3 ,4});            
+            var pos = new JointValues(new JointSet("a", "b", "c"), new double[] { 2, 3, 4 });
+            var vel = new JointValues(new JointSet("a", "b", "c"), new double[] { 2, 3, 4 });
+            var eff = new JointValues(new JointSet("a", "b", "c"), new double[] { 2, 3, 4 });
 
             var js = new JointStates(pos, vel, eff);
             Assert.NotNull(js.JointSet);
 
             Assert.Null((new JointStates(null, null, null)).JointSet);
             // all jointset must match
-            var badEff = new JointValues(new JointSet("f" , "b","c"), new double[] {2, 3 ,4});      
+            var badEff = new JointValues(new JointSet("f", "b", "c"), new double[] { 2, 3, 4 });
             Assert.Throws<System.Exception>(() => new JointStates(pos, vel, badEff));
 
         }
