3ca40ff7600e5a206652fbcad2f88a6169bae605
Alexandre Stueben <Alexandre.Stueben@mailbox.org>
2018-11-02 17:31:53 +0100

    Added test for merge for class JointTrajectoryPoints

--- Xamla.Robotics.Types.Tests/JointTrajectoryPointTests.cs
+++ Xamla.Robotics.Types.Tests/JointTrajectoryPointTests.cs
@@ -10,10 +10,11 @@ namespace Xamla.Robotics.Types.Tests
         private static JointTrajectoryPoint GetPoint()
         {
             var timeSpan = new TimeSpan(100);
-            var pos = new JointValues(new JointSet("a", "b", "c"), new double[] { 0, 1, 2 });
-            var vel = new JointValues(new JointSet("a", "b", "c"), new double[] { 3, 4, 5 });
-            var acc = new JointValues(new JointSet("a", "b", "c"), new double[] { 6, 7, 8 });
-            var eff = new JointValues(new JointSet("a", "b", "c"), new double[] { 9, 10, 11 });
+            var jointSet = new JointSet("a", "b", "c");
+            var pos = new JointValues(jointSet, new double[] { 0, 1, 2 });
+            var vel = new JointValues(jointSet, new double[] { 3, 4, 5 });
+            var acc = new JointValues(jointSet, new double[] { 6, 7, 8 });
+            var eff = new JointValues(jointSet, new double[] { 9, 10, 11 });
             var p = new JointTrajectoryPoint(timeSpan, pos, vel, acc, eff);
             return p;
         }
@@ -21,7 +22,7 @@ namespace Xamla.Robotics.Types.Tests
         [Fact]
         public void TestInit()
         {
-            var timeSpan = new TimeSpan(100);   
+            var timeSpan = new TimeSpan(100);
             var p = GetPoint();
             Assert.Equal(timeSpan, p.TimeFromStart);
         }
@@ -33,6 +34,23 @@ namespace Xamla.Robotics.Types.Tests
             var p = GetPoint();
             var q = p.AddTimeOffset(timeSpan);
             Assert.Equal(q.TimeFromStart - timeSpan, p.TimeFromStart);
+        }
+
+        [Fact]
+        public void TestMerge()
+        {
+            var p1 = GetPoint();
+            var timeSpan = p1.TimeFromStart;
+            var falseTimeSpan = new TimeSpan(200);
+            var jointSet = new JointSet("d", "e");
+            var pos = new JointValues(jointSet, new double[] { 0, 1 });
+            var vel = new JointValues(jointSet, new double[] { 3, 4 });
+            var acc = new JointValues(jointSet, new double[] { 6, 7 });
+            var eff = new JointValues(jointSet, new double[] { 9, 10 });
+            var p2 = new JointTrajectoryPoint(timeSpan, pos, vel, acc, eff);
+            var p = p1.Merge(p2);
+            var p3 = new JointTrajectoryPoint(falseTimeSpan, pos, vel, acc, eff);
+            Assert.Throws<Exception>(() => p1.Merge(p3) );
         }
     }
 }
